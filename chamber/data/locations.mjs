d8b797b4422a5c60deff3ad3c0244ae94fa3e512
johnvak01 <johnvak01@gmail.com>
2026-02-06 17:55:07 -0700

    updated data for local sites

---
+++ chamber/data/locations.mjs
@@ -0,0 +1,53 @@
+const location_data = {
+    "locations":[
+        {
+            "image":"dimple-dell.webp",
+            "name":"Dimple Dell Regional Park",
+            "address":"10600 S 1300 E, Sandy, UT 84092",
+            "description":"An Expansive Park with horse, hiking, and biking trails."
+        },
+        {
+            "image":"sandy-museum.webp",
+            "name":"Sandy Museum",
+            "address":"8744 S Center St, Sandy, UT 84070",
+            "description":"A Local History Museum housed in a local historic building."
+        },
+        {
+            "image":"sandy-amphitheater.webp",
+            "name":"Sandy Amphitheater",
+            "address":"1245 E 9400 S, Sandy, UT 84094",
+            "description":"An open-air venue for concerts, plays, and other performances."
+        },
+        {
+            "image":"hale-theater.webp",
+            "name":"Hale Center Theater",
+            "address":"address",
+            "description":"A closed air venue for the fine performing arts."
+        },
+        {
+            "image":"dog-park.webp",
+            "name":"Sandy City Dog Park",
+            "address":"9980 S 300 E, Sandy, UT 84070",
+            "description":"An active local dog park with doggy water fountain and track."
+        },
+        {
+            "image":"sego-lily.webp",
+            "name":"Sego Lily Gardens",
+            "address":"1472 Sego Lily Dr, Sandy, UT 84092",
+            "description":"description"
+        },
+        {
+            "image":"source",
+            "name":"name",
+            "address":"address",
+            "description":"description"
+        },
+        {
+            "image":"source",
+            "name":"name",
+            "address":"address",
+            "description":"description"
+        },
+    ]
+};
+export default location_data;
