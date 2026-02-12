41e2fdbbb1858ef7a965505066b7bd6599062d76
johnvak01 <johnvak01@gmail.com>
2026-02-11 19:39:26 -0700

    added scripting for modal

--- final_project/scripts/generate.js
+++ final_project/scripts/generate.js
@@ -1,30 +1,31 @@
 
+const title = document.getElementById("article-title");
+const sub_title = document.getElementById("article-sub-title");
+const author = document.getElementById("article-author");
+const content = document.getElementById("article-content");
+
 function saveEntry() {
     // load localstorage
     let articles = JSON.parse(localStorage.getItem("entry_data"));
     console.log(articles);
 
-    if(articles == null){
-        articles={"data":[]};
+    if (articles == null) {
+        articles = { "data": [] };
     }
     // Save the Article
     // set ID 
-    let id=0;
-    for(let x = 0; x<articles.data.length;x++){
-        if(id < articles.data[x].id){
+    let id = 0;
+    for (let x = 0; x < articles.data.length; x++) {
+        if (id < articles.data[x].id) {
             break;
-        }else{
+        } else {
             id++;
         }
     }
     const id_number = document.getElementById("id-number");
     id_number.value = id;
     // // get other elements of the form
-    const title = document.getElementById("article-title");
-    const sub_title = document.getElementById("article-sub-title");
-    const author = document.getElementById("article-author");
-    const content = document.getElementById("article-content");
-    
+
 
     let new_entry = {
         "id": id,
@@ -35,10 +36,48 @@ function saveEntry() {
     }
     // save to localstorage 
     articles.data.push(new_entry);
-    localStorage.setItem("entry_data",JSON.stringify(articles));
+    localStorage.setItem("entry_data", JSON.stringify(articles));
 
     // alter form to pass only the ID number to Browse page
 
     alert("pause");
     return true;
 }
+
+function hideModal() {
+    const modal = document.querySelector("dialog");
+    modal.close();
+    modal.innerHTML = "";
+}
+function PreviewEntry() {
+    console.log("preview");
+    const modal = document.querySelector("dialog");
+    const modal_title = document.createElement("h2");
+    const modal_sub_title = document.createElement("h3");
+    const modal_author = document.createElement("h4");
+    const modal_content = document.createElement("p");
+    const modal_close = document.createElement("div");
+
+    modal_title.textContent = title.value;
+    modal_sub_title.textContent = sub_title.value;
+    modal_author.textContent = author.value;
+    modal_content.textContent = content.value;
+
+    modal.appendChild(modal_title);
+    modal.appendChild(modal_sub_title);
+    modal.appendChild(modal_author);
+    modal.appendChild(modal_content);
+
+    modal_close.innerText = "X";
+    modal_close.setAttribute("id", "modal-close");
+    modal_close.addEventListener("click", () => { hideModal(); });
+    modal.appendChild(modal_close);
+
+    modal.showModal();
+}
+
+const preview = document.getElementById("button-preview");
+preview.addEventListener("click", () => {
+    console.log("test");
+    PreviewEntry();
+});
